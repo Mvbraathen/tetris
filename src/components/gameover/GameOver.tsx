0d1cf50599427e748e563dcfced6b283fc62cf14
Axel Boix Escolan <axbo@computas.com>
2022-03-15 14:14:31 +0100

    changed score-page design, wip

--- src/components/gameover/GameOver.tsx
+++ src/components/gameover/GameOver.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import css from './GameOver.module.scss';
 import ScorePage from './scorepage/ScorePage';
@@ -8,27 +8,51 @@ interface GameOverProps {
   score: number;
 }
 
+interface HighScores {
+  score: number;
+  name: string;
+}
+
 const GameOver = (props: GameOverProps) => {
   const { gameOver, score } = props;
   const [text, setText] = useState(<span>GAME OVER</span>);
-  const highScore = <ScorePage score={score} />;
+  const [currentRank, setCurrentRank] = useState(1);
+
+  const currentHighScores = JSON.parse(
+    localStorage.getItem('highScores') ?? '[]'
+  ) as HighScores[];
+
+  const highScore = (
+    <ScorePage
+      score={score}
+      rank={`${currentRank}/${currentHighScores.length}`}
+    />
+  );
+  useEffect(() => {
+    if (gameOver) {
+      if (currentHighScores.length === 0) {
+        const newScore = [{ score, name: 'Player' }];
+        localStorage.setItem('highScores', JSON.stringify(newScore));
+      } else {
+        currentHighScores.push({ score, name: 'Player' });
+        currentHighScores.sort(
+          (a: HighScores, b: HighScores) => b.score - a.score
+        );
+        localStorage.setItem('highScores', JSON.stringify(currentHighScores));
+        setCurrentRank(
+          currentHighScores.findIndex((item) => item.score <= score) + 1
+        );
+      }
+      setText(<span>GAME OVER</span>);
+      setTimeout(() => {
+        setText(highScore);
+      }, 2000);
+    }
+  }, [gameOver]);
 
   if (!gameOver) {
     return null;
   }
-
-  const currentHighScore = localStorage.getItem('highScore');
-  if (currentHighScore === null) {
-    localStorage.setItem('highScore', score.toString());
-  } else {
-    if (score > parseInt(currentHighScore, 10)) {
-      localStorage.setItem('highScore', score.toString());
-    }
-  }
-
-  setTimeout(() => {
-    setText(highScore);
-  }, 2000);
 
   return <div className={css.GameOver}>{text}</div>;
 };
--- src/components/gameover/scorepage/ScorePage.tsx
+++ src/components/gameover/scorepage/ScorePage.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { ReactComponent as ComputasLogo } from '../../../svg/computas.svg';
 import css from './ScorePage.module.scss';
 
 interface ScorePageProps {
   score: number;
+  rank: string;
 }
 
 const ScorePage = (props: ScorePageProps) => {
-  const { score } = props;
-  const highScore = localStorage.getItem('highScore');
-  const currentHighScore = highScore === null ? 0 : parseInt(highScore, 10);
-  if (score > currentHighScore) {
-    localStorage.setItem('highScore', score.toString());
-  }
-
-  const hiScore = () => {
-    console.log('hello world');
-  };
+  const { score, rank } = props;
 
   return (
     <div className={css.ScorePage}>
-      <div className={css.ScorePage__score}>
-        <span>Poeng: {score}</span>
-        <span>High Score: {highScore}</span>
+      <ComputasLogo
+        style={{
+          width: '210px',
+          height: '210px',
+          filter: 'invert(100%) grayscale(100%) contrast(200%)'
+        }}
+      />
+      <div className={css.ScorePageScore}>
+        <span>
+          Poengsum: <br />
+          {score}
+        </span>
       </div>
-      <div className={css.ScorePage__button}>
-        <button onClick={() => hiScore()}>Se toppliste</button>
+      <div className={css.ScorePageRank}>
+        <span>Plassering: {rank}</span>
+      </div>
+      <div className={css.ScorePageButton}>
+        <button>Toppliste</button>
       </div>
     </div>
   );
